0357b6727f2b7206ad2ad9d73cfd590c38ed987a
Adam Janicki <adamjanicki2@gmail.com>
2024-08-16 17:15:03 -0700

    select element container

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -36,6 +36,14 @@ type Props = Omit<
    * Name of the select for accessibility purposes
    */
   "aria-label": string;
+  /**
+   * [Optional] The styles to apply to the select element
+   */
+  innerStyle?: React.CSSProperties;
+  /**
+   * [Optional] The class name to apply to the select element
+   */
+  innerClassName?: string;
 };
 
 const identity = (x: string) => x;
@@ -47,21 +55,51 @@ const Select = (
     getOptionValue = identity,
     getOptionLabel = identity,
     corners = "rounded",
+    style = {},
+    innerStyle,
+    innerClassName,
+    disabled,
     ...props
   }: Props,
   ref: React.Ref<HTMLSelectElement>
 ) => (
-  <select
-    {...props}
-    ref={ref}
-    className={classNames(`ajui-select-base corners--${corners}`, className)}
+  <div
+    className={
+      classNames(
+        "ajui-select-container",
+        disabled ? "ajui-select-disabled" : undefined,
+        className
+      ) || undefined
+    }
   >
-    {options.map((option, index) => (
-      <option key={index} value={getOptionValue(option)}>
-        {getOptionLabel(option)}
-      </option>
-    ))}
-  </select>
+    <select
+      {...props}
+      ref={ref}
+      className={classNames(
+        `ajui-select-base corners--${corners}`,
+        innerClassName
+      )}
+      style={innerStyle}
+      disabled={disabled}
+    >
+      {options.map((option, index) => (
+        <option key={index} value={getOptionValue(option)}>
+          {getOptionLabel(option)}
+        </option>
+      ))}
+    </select>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 64 64"
+      className="ajui-select-triangle"
+      aria-hidden="true"
+    >
+      <path
+        fill="currentColor"
+        d="M 29.175781 50.824219 C 30.738281 52.386719 33.273438 52.386719 34.835938 50.824219 L 58.835938 26.824219 C 60.398438 25.261719 60.398438 22.726562 58.835938 21.164062 C 57.273438 19.601562 54.738281 19.601562 53.175781 21.164062 L 32 42.335938 L 10.824219 21.175781 C 9.261719 19.613281 6.726562 19.613281 5.164062 21.175781 C 3.601562 22.738281 3.601562 25.273438 5.164062 26.835938 L 29.164062 50.835938 Z M 29.175781 50.824219"
+      />
+    </svg>
+  </div>
 );
 
 export default forwardRef(Select);
