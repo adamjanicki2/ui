d28ad99c8a18cd8657454463dee249a5d0c9cfd9
Adam Janicki <adamjanicki2@gmail.com>
2024-09-06 20:01:17 -0700

    bump

--- src/hooks/useScrollToHash.ts
+++ src/hooks/useScrollToHash.ts
@@ -4,30 +4,35 @@ import scrollToId from "../functions/scrollToId";
 type UseScrollToHashConfig = {
   /**
    * Whether or not to scroll to the hash
+   * @default true
    */
-  active: boolean;
+  active?: boolean;
   /**
    * The scroll behavior to use
    */
   behavior?: ScrollBehavior;
+  /**
+   * Delay in ms to set using setTimeout
+   */
+  delay?: number;
 };
 
 /**
  * A hook for scrolling to a hash on the page.
  */
-const useScrollToHash = ({
-  active,
-  behavior = "smooth",
-}: UseScrollToHashConfig) => {
+const useScrollToHash = (config: UseScrollToHashConfig = {}) => {
+  const { active = true, behavior, delay } = config;
   useEffect(() => {
-    if (!active) return;
-
     const hash = window.location.hash;
-    if (hash?.length > 1) {
-      const id = hash.substring(1);
-      scrollToId(id, behavior);
+    if (!active || hash?.length <= 1) return;
+    const id = hash.substring(1);
+    scrollToId(id, behavior);
+    if (delay !== undefined) {
+      const timeout = setTimeout(() => scrollToId(id, behavior), delay);
+      return () => clearTimeout(timeout);
     }
-  }, [active, behavior]);
+    scrollToId(id, behavior);
+  }, [active, behavior, delay]);
 };
 
 export default useScrollToHash;
