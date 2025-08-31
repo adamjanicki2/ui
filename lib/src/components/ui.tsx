4cf4547a2e345a6fad6f83e658adb5849771583f
Adam Janicki <adamjanicki2@gmail.com>
2025-08-30 20:38:12 -0700

    cash in

--- lib/src/components/ui.tsx
+++ lib/src/components/ui.tsx
@@ -3,16 +3,17 @@ import type { Layout } from "../utils/types";
 import { classNames } from "../functions";
 import transformLayout from "../utils/transformLayout";
 
-type Props<T extends keyof JSX.IntrinsicElements> =
-  React.ComponentPropsWithRef<T> & {
-    /**
-     * The organization of this element and its children.
-     * Properties are translated to class names before being applied.
-     */
-    layout?: Layout;
-  };
+type HtmlTag = keyof React.JSX.IntrinsicElements;
 
-function createLayoutElement<T extends keyof JSX.IntrinsicElements>(tag: T) {
+type Props<T extends HtmlTag> = React.ComponentPropsWithRef<T> & {
+  /**
+   * The organization of this element and its children.
+   * Properties are translated to class names before being applied.
+   */
+  layout?: Layout;
+};
+
+function createLayoutElement<T extends HtmlTag>(tag: T) {
   const Component = React.forwardRef<React.ComponentRef<T>, Props<T>>(
     ({ layout, className, ...props }, ref) =>
       React.createElement(tag, {
@@ -25,36 +26,23 @@ function createLayoutElement<T extends keyof JSX.IntrinsicElements>(tag: T) {
   return Component;
 }
 
-const ui = {
-  div: createLayoutElement("div"),
-  span: createLayoutElement("span"),
-  nav: createLayoutElement("nav"),
-  footer: createLayoutElement("footer"),
-  p: createLayoutElement("p"),
-  br: createLayoutElement("br"),
-  button: createLayoutElement("button"),
-  pre: createLayoutElement("pre"),
-  code: createLayoutElement("code"),
-  em: createLayoutElement("em"),
-  strong: createLayoutElement("strong"),
-  blockquote: createLayoutElement("blockquote"),
-  ul: createLayoutElement("ul"),
-  ol: createLayoutElement("ol"),
-  li: createLayoutElement("li"),
-  table: createLayoutElement("table"),
-  tr: createLayoutElement("tr"),
-  th: createLayoutElement("th"),
-  td: createLayoutElement("td"),
-  tbody: createLayoutElement("tbody"),
-  iframe: createLayoutElement("iframe"),
-  section: createLayoutElement("section"),
-  main: createLayoutElement("main"),
-  h1: createLayoutElement("h1"),
-  h2: createLayoutElement("h2"),
-  h3: createLayoutElement("h3"),
-  h4: createLayoutElement("h4"),
-  h5: createLayoutElement("h5"),
-  h6: createLayoutElement("h6"),
-} as const;
+type HtmlTags = {
+  [K in HtmlTag]: ReturnType<typeof createLayoutElement<K>>;
+};
+
+const ui = new Proxy(
+  {},
+  {
+    get: (cache: Partial<HtmlTags>, tag: HtmlTag) => {
+      const cachedComponent = cache[tag];
+      if (cachedComponent) {
+        return cachedComponent;
+      }
+      const component = createLayoutElement(tag);
+      cache[tag] = component;
+      return component;
+    },
+  }
+) as HtmlTags;
 
 export default ui;
