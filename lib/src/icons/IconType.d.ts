661b7a7394797f03d8bf6b32f4b26e80513a87d0
Adam Janicki <adamjanicki2@gmail.com>
2025-12-30 23:53:27 -0500

    comment

--- lib/src/icons/IconType.d.ts
+++ lib/src/icons/IconType.d.ts
@@ -1,2 +1,3 @@
-declare const auiIcon: unique symbol;
-export type IconType = string & { readonly [auiIcon]: "auiIcon" };
+// hacky type to loosely enforce icon paths are imported from icons/
+declare const brand: unique symbol;
+export type IconType = string & { readonly [brand]: true };
