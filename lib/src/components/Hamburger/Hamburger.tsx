f547b827643920ea97ddb2d8d47656af0ad184ae
Adam Janicki <45052692+adamjanicki2@users.noreply.github.com>
2026-01-13 20:41:32 -0800

    [cleanup] cut down on build size (#89)
    * compact hamburger
    
    * little more cleanup

--- lib/src/components/Hamburger/Hamburger.tsx
+++ lib/src/components/Hamburger/Hamburger.tsx
@@ -1,18 +1,21 @@
 import { useMemo } from "react";
 
 import type { Style } from "../../types/common";
+import { DEFAULT_ANIMATION_DURATION_S } from "../Animated/Animated";
 import Box from "../Box";
 import { UnstyledButton } from "../Button";
 
+type ButtonProps = React.ComponentProps<typeof UnstyledButton>;
+
 /** Props shared by all the hamburgers */
-export type Props = {
+type Props = Omit<ButtonProps, "children" | "aria-expanded"> & {
   /**
    * Size of the button in pixels.
    * @default 36
    */
   size?: number;
-  /** Line height of the bars in pixels */
-  lineHeight?: number;
+  /** Height of the bars in pixels */
+  barHeight?: number;
   /**
    * Direction the animation originates from.
    * Play around with this to see how it affects the animation because some of the animations are complicated.
@@ -21,26 +24,12 @@ export type Props = {
   direction?: "left" | "right";
   /** Whether the button is open or closed */
   open: boolean;
-  /** Function to call when the button is clicked, usually should toggle the `open` state */
-  onClick?: () => void;
-  /**
-   * Aria label for the button.
-   * @default "hamburger"
-   */
-  "aria-label"?: string;
   /**
    * Duration of the animation in seconds.
    * @default 0.25
    */
   duration?: number;
-  /** Additional class name to apply to the button */
-  className?: string;
-  /** Additional styles to apply to the button */
-  style?: Style;
-  /**
-   * Round borders of the bars.
-   * @default false
-   */
+  /** Whether to round the borders of the bars */
   rounded?: boolean;
 };
 
@@ -57,13 +46,13 @@ type InnerProps = Omit<Props, "variant" | "direction"> & {
 };
 
 /** Default angles for rotate-style hamburger animations */
-export const defaultAngles = {
+const defaultAngles = {
   right: 45,
   left: -45,
 } as const;
 
 /** Alternate angles for flip-style hamburger animations */
-export const flipAngles = {
+const flipAngles = {
   right: -135,
   left: -225,
 } as const;
@@ -72,16 +61,16 @@ export const flipAngles = {
 const Hamburger = (props: InnerProps) => {
   const {
     size = 36,
-    duration = 0.25,
+    duration = DEFAULT_ANIMATION_DURATION_S,
     double,
     openStyle,
     open,
     style,
     rounded,
-    lineHeight: inputLineHeight,
+    barHeight: inputBarHeight,
     ...buttonProps
   } = props;
-  const lineHeight = inputLineHeight || Math.max(1, Math.round(size / 20));
+  const barHeight = inputBarHeight || Math.max(1, Math.round(size / 20));
 
   const buttonStyle: Style = {
     width: size,
@@ -90,46 +79,106 @@ const Hamburger = (props: InnerProps) => {
     position: "relative",
     justifyContent: "center",
     alignItems: "center",
-    transition: `transform ${duration}s ease`,
+    transition: `transform ${duration}s`,
   };
 
-  const commonLineStyle: Style = {
-    height: lineHeight,
+  const commonBarStyle: Style = {
+    height: barHeight,
     width: size * 0.7,
     backgroundColor: "currentColor",
     position: "absolute",
-    transition: `transform ${duration}s ease, opacity ${duration}s ease`,
+    transition: `transform ${duration}s, opacity ${duration}s`,
     borderRadius: rounded ? 10000 : 0,
   };
 
-  const yTranslateMagnitude = useMemo(
-    () => size / (double ? 7 : 4),
-    [size, double]
-  );
+  const yMag = size / (double ? 7 : 4);
 
   const { outer, top, middle, bottom } = openStyle;
 
   const outerStyle = open ? outer : {};
-  const topStyle = open
-    ? top
-    : { transform: `translateY(-${yTranslateMagnitude}px)` };
-  const bottomStyle = open
-    ? bottom
-    : { transform: `translateY(${yTranslateMagnitude}px)` };
+  const topStyle = open ? top : { transform: `translateY(-${yMag}px)` };
+  const bottomStyle = open ? bottom : { transform: `translateY(${yMag}px)` };
   const middleStyle = open ? middle : {};
 
   return (
     <UnstyledButton
-      style={{ ...style, ...outerStyle, ...buttonStyle }}
       {...buttonProps}
-      aria-label={buttonProps["aria-label"] || "hamburger"}
       aria-expanded={open}
+      style={{ ...style, ...outerStyle, ...buttonStyle }}
     >
-      <Box style={{ ...commonLineStyle, ...topStyle }} />
-      {!double && <Box style={{ ...commonLineStyle, ...middleStyle }} />}
-      <Box style={{ ...commonLineStyle, ...bottomStyle }} />
+      <Box style={{ ...commonBarStyle, ...topStyle }} />
+      {!double && <Box style={{ ...commonBarStyle, ...middleStyle }} />}
+      <Box style={{ ...commonBarStyle, ...bottomStyle }} />
     </UnstyledButton>
   );
 };
 
-export default Hamburger;
+type HamburgerConfig = {
+  angles: typeof defaultAngles | typeof flipAngles;
+  double?: boolean;
+  middle?: Style;
+  outer?: Style;
+};
+
+const makeHamburger =
+  ({ angles, double, middle, outer }: HamburgerConfig) =>
+  (props: Props) => {
+    const { direction = "left", ...rest } = props;
+
+    const openStyle = useMemo(() => {
+      const angle = angles[direction];
+
+      const style: OpenStyle = {
+        top: { transform: `rotate(${angle}deg)` },
+        bottom: { transform: `rotate(${-angle}deg)` },
+      };
+
+      if (middle) style.middle = middle;
+      if (outer) style.outer = outer;
+
+      return style;
+    }, [direction]);
+
+    return <Hamburger {...rest} double={double} openStyle={openStyle} />;
+  };
+
+/** A double-bar "cross" hamburger */
+export const DoubleCross = makeHamburger({
+  angles: defaultAngles,
+  double: true,
+});
+
+/** A double-bar "flip" hamburger */
+export const DoubleFlip = makeHamburger({ angles: flipAngles, double: true });
+
+/** A double-bar "spin" hamburger */
+export const DoubleSpin = makeHamburger({
+  angles: defaultAngles,
+  double: true,
+  outer: { transform: "rotate(180deg)" },
+});
+
+/** A three-bar "spin" hamburger */
+export const TripleSpin = makeHamburger({
+  angles: defaultAngles,
+  middle: { opacity: 0 },
+  outer: { transform: "rotate(180deg)" },
+});
+
+/** A three-bar "flip" hamburger */
+export const TripleFlip = makeHamburger({
+  angles: flipAngles,
+  middle: { transform: "scale(0)" },
+});
+
+/** A three-bar hamburger that fades the middle bar */
+export const TripleFade = makeHamburger({
+  angles: defaultAngles,
+  middle: { opacity: 0 },
+});
+
+/** The default hamburger */
+export const TriplePrestige = makeHamburger({
+  angles: defaultAngles,
+  middle: { transform: "scale(0)" },
+});
