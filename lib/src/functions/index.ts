cb1f417690c72d3d1c097c6b12d7c9e3947cdab1
Adam Janicki <adamjanicki2@gmail.com>
2026-02-04 20:07:29 -0800

    shed useless function

--- lib/src/functions/index.ts
+++ lib/src/functions/index.ts
@@ -1,4 +1,3 @@
-export { default as assertDefined } from "./assertDefined";
 export { default as classNames } from "./classNames";
 export { default as getDeviceType } from "./getDeviceType";
 export { default as scrollToId } from "./scrollToId";
