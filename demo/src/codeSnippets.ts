cb40feec84361243123d596c0cb3864770f0737f
Adam Janicki <adamjanicki2@gmail.com>
2026-02-05 17:24:25 -0800

    delete modal

--- demo/src/codeSnippets.ts
+++ demo/src/codeSnippets.ts
@@ -155,19 +155,6 @@ export const layerSnippet = `
 </Layer>
 `;
 
-export const modalSnippet = `
-<Modal
-  open={modalOpen}
-  onClose={() => setModalOpen(false)}
-  onConfirm={() => {}}
->
-  <Box>
-    Welcome to my modal. You can put all sorts of stuff in here if
-    you'd like.
-  </Box>
-</Modal>
-`;
-
 export const linkSnippet = `
 <Link to="#link">Internal link</Link>
 <Link to="https://adamovies.com" newTab>External link</Link>
